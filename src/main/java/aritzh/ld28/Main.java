c416f4a997f01b1461d58723045f1fe07de6d5ca
aritzhack <aritzhack@gmail.com>
2013-12-14 08:45:14 +0100

    * Game will now be square, since the board will be like that (actual size is subject to change, though)

--- src/main/java/aritzh/ld28/Main.java
+++ src/main/java/aritzh/ld28/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game game = new Game(800, 600, false);
+        Game game = new Game(800, 800, false);
 
         game.start();
 
