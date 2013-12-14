d6012da21f3006e8a7feb51b601ef4e56ed885e0
aritzhack <aritzhack@gmail.com>
2013-12-14 09:25:19 +0100

    * Set final window size

--- src/main/java/aritzh/ld28/Main.java
+++ src/main/java/aritzh/ld28/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Game game = Game.getGame(800, 800, false);
+        Game game = Game.getGame(750, 750, false);
         game.start();
 
     }
