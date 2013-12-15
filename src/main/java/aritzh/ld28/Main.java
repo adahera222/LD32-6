acaeac6597d72f33a444b437a261ce3abc1a97b1
aritzhack <aritzhack@gmail.com>
2013-12-15 10:39:12 +0100

    * Merged Applet and Main class

--- src/main/java/aritzh/ld28/Main.java
+++ src/main/java/aritzh/ld28/Main.java
@@ -1,13 +1,35 @@
 package aritzh.ld28;
 
+import java.applet.Applet;
+
 /**
  * @author Aritz Lopez
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
-public class Main {
+public class Main extends Applet {
+
+    private Game game;
+    private static final int WIDTH = 602, HEIGHT = 652;
+
+    public Main() {
+        this.game = Game.getGame(WIDTH, HEIGHT, true);
+    }
 
     public static void main(String[] args) {
-        Game game = Game.getGame(602, 652, false);
+        Game game = Game.getGame(WIDTH, HEIGHT, false);
         game.start();
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        this.add(game);
+        this.game.start();
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+        this.game.stop();
     }
 }
